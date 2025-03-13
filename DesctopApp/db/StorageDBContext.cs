2ca398bd3a68f7f173198369e55c80d587fdc93d
Student <student@itstep.pl>
2025-03-13 20:44:08 +0200

    added db kernel

---
+++ DesctopApp/db/StorageDBContext.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DesctopApp.db
+{
+    internal class StorageDBContext
+    {
+        string ConnectionString { get; set; }
+        
+        string DatabaseName { get; set; }
+
+        List<string> TableNames { get; set; }
+
+        LinkedList<Object> Tables { get; set; }
+
+        public StorageDBContext()
+        {
+            
+        }
+
+    }
+}
